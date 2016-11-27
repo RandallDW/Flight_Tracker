9d148a1c9a6720dd9d5e4a9ca6e6e49cd2deb5a5
Randall <dong0125@vt.edu>
2016-11-26 21:50:59 -0500

    add flight info checking

--- server.py
+++ server.py
@@ -28,39 +28,85 @@ class Weather(object):
 		request = url + self.location + api_key
 		weather = requests.get(request)
 		self.weather_text = weather.text
-
 """
 
+"""
+class flightInfo(object):
+	def __init__(self, origin, destination, date):
+		self.origin = origin
+		self.destination = destination
+		self.date = date
+		self.getInfo()
+	def getInfo(self):
+		api_key = "AIzaSyCXUADGIAa5BkzzRUh8CbIWFFAzKYoTCd4"
+		url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + api_key
+		params = {
+		  "request": {
+		    "slice": [
+		      {
+		        "origin": self.origin,
+		        "destination": self.destination,
+		        "date": self.date
+		      }
+		    ],
+		    "passengers": {
+		      "adultCount": 1
+		    },
+		    "solutions": 1,
+		    "refundable": False
+		  }
+		}
+		response = requests.post(url, data=json.dumps(params), headers=headers)
+		data = response.json()
+		trips_text = data.get('trips')
+		self.trips_data = trips_text.get('data')
+
+		if self.trips_data.get('airport') == None:
+			self.info = None
+		else:
+			self.info = self.trips_data 
+
+"""
+client thread, 
 """
 class ClientThread(threading.Thread) :
 
 	def __init__(self,clientsocket):
 		threading.Thread.__init__(self)
 		self.csocket = clientsocket
+		self.size = 1024
 		#self.id = id
 		print ("[+] New thread started ")
 
 	def run(self): 
+		question_byte = self.csocket.recv(self.size)
+		question_str  = decode(question_byte, 'utf-8')
+		print(question_str)
+		"""
 		count = 0
 		while True:
 			time.sleep(1)
 			#print("Id is " + str(self.id))
+			
+			print(data)
 			count += 1
 			if (count == 5):
 				self.csocket.close()
 				self.csocket = None
 				break
-				
+		"""
 """
-
+LED thread, control 7 segement LED show coresponding client number
 """
 class LEDThread(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
+		self.num = 0;
 	def run(self):
 		while 1:
 			time.sleep(1)
-			print(threading.active_count() - 2)
+			self.num = threading.active_count() - 2
+			print(self.num)
 		
 			#print(Server.backlog)
 			#if (Server.client[0] == None):
@@ -68,7 +114,7 @@ class LEDThread(threading.Thread):
 	#def count_1(self):
 
 """
-
+server 
 """
 class Server(object):
 	def __init__(self, hostAdd):
@@ -117,13 +163,3 @@ if __name__ == "__main__":
 
 	weather = Weather('ROA')
 	print(weather.weather_text)
-	#airport_code = 'ROA'
-	#data = requests.get( + airport_code)
-	
-	#print(type(data))
-	
-	#print (location)
-
-#	weather = requests.get(Blacksburg)
-
-#	print (weather.text)
