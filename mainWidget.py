2a71dd42fe0a9eaa34d64f5e56a8de464396fda9
Randall <dong0125@vt.edu>
2016-12-06 19:20:06 -0500

    add background graph

--- mainWidget.py
+++ mainWidget.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import socket
 import time
 import _thread
@@ -10,9 +10,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class MainWidget(QTabWidget):
+	submit = pyqtSignal()
 	def __init__(self, parent = None):
 		super(MainWidget, self).__init__(parent) 
-		self.setGeometry(400,100,800,800)
+		self.setGeometry(100,100,1200,800)
 		self.tab1 = QWidget()
 		self.tab2 = QWidget()
 		self.tab3 = QWidget()
@@ -21,10 +22,11 @@ class MainWidget(QTabWidget):
 		self.addTab(self.tab1, "Search")
 		self.addTab(self.tab2, "Destination Weather")
 		self.addTab(self.tab3, "Solutions")
+		self.addTab(self.tab4, "Flight Status")
 		self.tab1UI()
 		self.tab2UI()		
 		self.tab3UI()
-
+		self.tab4UI()
 		self.setWindowTitle("Network Application Design")
 		self.show()
 
@@ -32,12 +34,9 @@ class MainWidget(QTabWidget):
 		layout = QFormLayout()
 		box_line_1 = QGridLayout()
 		box_line_2 = QGridLayout()
-		
-
-
 
 		# first layout box
-		name = QLabel("Name:")
+		name = QLabel("Current Address:")
 		dest_code = QLabel("Destination Airport Code:")
 		self.name_line_edit = QLineEdit()
 		self.dest_line_edit = QLineEdit()
@@ -71,10 +70,24 @@ class MainWidget(QTabWidget):
 		self.submit_button.setText("Submit")
 		box_line_2.addWidget(self.submit_button, 1, 6)
 		
+
+		# self.invalid label
+		font = QFont()
+		font.setPointSize(20)
+		self.err_msg = QLabel('')
+		self.err_msg.setFont(font)
+
+
 		# add layout
 		layout.addRow(box_line_1)
 		layout.addRow(box_line_2)
+		layout.addRow(self.err_msg)
 		
+		pic = QLabel(self.tab1)
+		pic.setGeometry(10, 10, 1200, 700)
+		#use full ABSOLUTE path to the image, not relative
+		pic.setPixmap(QPixmap(os.getcwd() + "/flight.jpg"))
+	
 		self.setTabText(0,"Flight searching")
 		self.tab1.setLayout(layout)
 
@@ -89,10 +102,8 @@ class MainWidget(QTabWidget):
 		self.weather_text_edit  = QTextEdit()
 		self.weather_text_edit.setReadOnly(True)
 		
-
 		layout.addWidget(weather_label, 0, 0)
 		layout.addWidget(self.weather_text_edit, 1, 0)
-		
 		
 		self.setTabText(1,"Destination Weather")
 		self.tab2.setLayout(layout)
@@ -115,21 +126,55 @@ class MainWidget(QTabWidget):
 		self.tab3.setLayout(layout)
 
 	def tab4UI(self):
-		layout = QGridLayout()
-		
-		label_1 = QLabel("Rover 1 Sent")
-		label_2 = QLabel("Rover 2 Received")
-		
-		self.rover_1_sent  = QTextEdit()
-		self.rover_1_received  = QTextEdit()
-		self.rover_1_received.setReadOnly(True)
-		self.rover_1_sent.setReadOnly(True)
+		layout = QFormLayout()
+		box_line_1 = QGridLayout()
+		box_line_2 = QGridLayout()
 
-		layout.addWidget(label_1, 0, 0)
-		layout.addWidget(label_2, 0, 1)
-		layout.addWidget(self.rover_1_sent, 1, 0)
-		layout.addWidget(self.rover_1_received, 1, 1)
+		# first layout box
+		name = QLabel("Current Address:")
+		dest_code = QLabel("Destination Airport Code:")
 
-		self.setTabText(3, "milestone 4")
+		self.setTabText(3, "Flight Status")
 		self.tab4.setLayout(layout)
 
+
+	'''
+	flight searching error msg
+	'''
+	def show_invalid_airport_msg(self):	
+		self.err_msg.setText('Error: Invalid airport code..')
+		self.err_msg.setStyleSheet('color: red')
+
+
+	def show_invalid_date_msg(self):
+		self.err_msg.setText('Error: Invalid date..')
+		self.err_msg.setStyleSheet('color: red')
+
+	def reset_error_msg_label(self):
+		self.err_msg.setText('')
+
+	def enter_address(self):
+		self.err_msg.setText('Error: Please enter current address')
+		self.err_msg.setStyleSheet('color: red')
+
+	def enter_airport_code(self):
+		self.err_msg.setText('Error: Please enter destination airport code')
+		self.err_msg.setStyleSheet('color: red')
+	def enter_date(self):
+		self.err_msg.setText('Error: Please enter date')
+		self.err_msg.setStyleSheet('color: red')
+	def enter_no_flight(self):
+		self.err_msg.setText('Error: No flight available..')
+		self.err_msg.setStyleSheet('color: red')
+
+	def set_weather_info(self, msg):
+		self.weather_text_edit.setText(msg)
+
+	def set_flight_info(self, flight, price):
+		self.solution_text_edit.setText("Flight Info:")
+		for i in range (0, len(flight)):
+			if flight[i] != None:
+				self.solution_text_edit.append("\t Solution# %d: Sale Price: %s" % (i + 1, price[i]) )
+				self.solution_text_edit.append("\t\tOrigin\tDestination\tDepartureTime\tArrivalTime\t\tAvailable Seat\t\tMeal")
+				for j in range (0, len(flight[i])):
+					self.solution_text_edit.append("\t\t" + (flight[i])[j])
